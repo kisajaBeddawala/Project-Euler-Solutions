6d82d1f6b761c124c5dc79f3885d68dffd194118
Kisaja Beddawala <kisajab72@gmail.com>
2025-08-29 17:14:58 +0530

    add comments to problem 4

--- Problem4.py
+++ Problem4.py
@@ -1,20 +1,21 @@
 #Largest Palindrome Product
 
+#function for checking number is palindrome or not
 def isPalindrome(num):
-    if str(num) == str(num)[::-1]:
+    if str(num) == str(num)[::-1]:   #convert number to string and use slicing to check palindrome or not
         return True
     else:
         return False
 
 def answer():
-    ans = []
+    ans = []        #store all palindrome number because we can't predict first one is always max 
     for i in range(999,99,-1):
-        if i % 10 != 0:
+        if i % 10 != 0:     #if some number has 0 or 00 as their last digit it can't palindrome 
             for j in range(999,99,-1):
                 if j % 10 != 0 and isPalindrome(i*j):
                     ans.append(i*j)
     
-    return max(ans)
+    return max(ans)     #return max number from list
                 
 print(answer())
                 

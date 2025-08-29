3a3e767d0435f51fbd87ddb488dcbc041a0ab7e6
Kisaja Beddawala <kisajab72@gmail.com>
2025-08-29 17:10:48 +0530

    add comment for problem 3

--- Problem3.py
+++ Problem3.py
@@ -1,11 +1,14 @@
+#Largest Prime Factor
+
+#import math library
 import math
 
 def isPrime(n):
     count = 0
-    for j in range(2,int(math.sqrt(n))+1):
+    for j in range(2,int(math.sqrt(n))+1):  #use square root of number to reduce time 
         if n % j == 0:
             count += 1
-    if count == 0:
+    if count == 0:  #if count is equal to zero it mean there is no other factor instead of 1 and itself
         return True
     return False
 
